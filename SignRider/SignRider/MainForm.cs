b6ce0a40a73a77393177810155b881755ae296fa
swkrueger <swk@swk.za.net>
2013-10-24 23:50:45 +0200

    EmguCV Hello World

--- SignRider/SignRider/MainForm.cs
+++ SignRider/SignRider/MainForm.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
 
 namespace SignRider
 {
@@ -15,6 +19,25 @@ namespace SignRider
         public MainForm()
         {
             InitializeComponent();
+            doStuff();
         }
+
+    private void doStuff()
+    {
+    //Create an image of 400x200 of Blue color
+using (Image<Bgr, Byte> img = new Image<Bgr, byte>(400, 200, new Bgr(255, 0, 0))) 
+{
+   //Create the font
+   MCvFont f = new MCvFont(FONT.CV_FONT_HERSHEY_COMPLEX, 1.0, 1.0);
+ 
+   //Draw "Hello, world." on the image using the specific font
+   img.Draw("Hello, world", ref f, new Point(10, 80), new Bgr(0, 255, 0)); 
+ 
+   //Show the image using ImageViewer from Emgu.CV.UI
+   ImageViewer.Show(img, "Test Window");
+}
+    }
+
+
     }
 }
