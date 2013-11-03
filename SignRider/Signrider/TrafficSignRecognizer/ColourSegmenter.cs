37720e539603f24f8e7ab6e3c22b79d3f7b42999
CJ van der Spuy <cjspuy@gmail.com>
2013-11-03 15:52:52 +0200

    Program cleanup and two contours on one crop fix
    red range change for darker red
    CYMK fully removed
    GC added

--- SignRider/Signrider/TrafficSignRecognizer/ColourSegmenter.cs
+++ SignRider/Signrider/TrafficSignRecognizer/ColourSegmenter.cs
@@ -29,13 +29,13 @@ namespace Signrider
         private int minimumSegmentWidth = 30;
         private int minimumSegmentHeight = 30;
         private int minimumAspectRatio = 2; //1:??
-        private enum SignNotFound { HSV, tryGammaCorrect, tryCMYK };
-        SignNotFound signNotFound = SignNotFound.HSV;
-        private Boolean isSignFound = false;
+        private enum SignNotFound { HSV, tryGammaCorrect};
 
 
         public List<ColourSegment> determineColourSegments(Image<Bgr, byte> image)
         {
+            Boolean isSignFound = false;
+            SignNotFound signNotFound = SignNotFound.HSV;
             List<ColourSegment> colourSegmentList = new List<ColourSegment>();
             foreach (SignColour colour in Enum.GetValues(typeof(SignColour)))
             {
@@ -55,15 +55,8 @@ namespace Signrider
                         imageGamma._GammaCorrect(2.2);
                         fullBinaryImage = GetPixelMask("HSV", colour, imageGamma);
                     }
-                    else if (signNotFound == SignNotFound.tryCMYK)
-                    {
-                        //Image<Bgr, byte> imageGamma = new Image<Bgr, byte>(image.Width, image.Height);
-                        //image.CopyTo(imageGamma);
-                        //imageGamma._GammaCorrect(2.2);
-                        fullBinaryImage = GetPixelMask("CMYK", colour, image);
-                    }
 
-                    Image<Gray, byte> mask = fullBinaryImage.CopyBlank();
+   
                     Image<Gray, byte> binaryCrop = null;
                     Image<Bgr, byte> rgbCrop = null;
 
@@ -87,6 +80,7 @@ namespace Signrider
 
                                 if (rWidth > minimumSegmentWidth && rHeight > minimumSegmentHeight && aspectRatio > 1 / (double)minimumAspectRatio && aspectRatio < minimumAspectRatio)//
                                 {
+                                    Image<Gray, byte> mask = fullBinaryImage.CopyBlank();
                                     mask.Draw(contour, new Gray(255), -1);
                                     binaryCrop = mask.Copy(rect);
                                     rgbCrop = image.Copy(rect);
@@ -104,13 +98,9 @@ namespace Signrider
                     }
                     else if (!isSignFound && signNotFound == SignNotFound.tryGammaCorrect)
                     {
-                        signNotFound = SignNotFound.tryCMYK;
                         isSignFound = true;
                     }
-                    //else if (!isSignFound && signNotFound == SignNotFound.tryCMYK)
-                    //{
-                    //    isSignFound = true;
-                    //}
+
 
                     // Free memory
                     //fullBinaryImage.Dispose();
@@ -126,144 +116,46 @@ namespace Signrider
         //-> function returning the binary image with white the segment and black not
         private Image<Gray, Byte> GetPixelMask(String ColourSpace, SignColour Colour, Image<Bgr, byte> image)
         {
-            if (ColourSpace == "HSV")
+            int StartRangeH = 0;
+            int EndRangeH = 0;
+            int StartRangeV = 0;
+            int StartRangeS = 0;
+            if (Colour == SignColour.BLUE)
             {
-                int StartRangeH = 0;
-                int EndRangeH = 0;
-                int StartRangeV = 0;
-                int StartRangeS = 0;
-                if (Colour == SignColour.BLUE)
-                {
-                    StartRangeH = 100;
-                    EndRangeH = 135;
-                    StartRangeS = 110;
-                    StartRangeV = 30;
-                }
-
-                if (Colour == SignColour.RED)
-                {
-                    StartRangeH = 10;
-                    EndRangeH = 170;
-                    StartRangeS = 50;
-                    StartRangeV = 30;
-                }
-                Image<Hsv, Byte> hsv = image.Convert<Hsv, Byte>();
-                Image<Gray, Byte>[] channels = hsv.Split();
-                CvInvoke.cvInRangeS(channels[0], new MCvScalar(StartRangeH), new MCvScalar(EndRangeH), channels[0]);
-                if (Colour == SignColour.RED)
-                {
-                    channels[0]._Not();
-                }
-                channels[1]._ThresholdBinary(new Gray(StartRangeS), new Gray(255.0));
-                channels[2]._ThresholdBinary(new Gray(StartRangeV), new Gray(255.0));
-                CvInvoke.cvAnd(channels[0], channels[1], channels[0], IntPtr.Zero);
-                CvInvoke.cvAnd(channels[0], channels[2], channels[0], IntPtr.Zero);
-                return channels[0];
-
-
-                //-> om een of ander rede gee dit nie dieselfde resultate nie
-                //Image<Gray, Byte> mask = channels[0].InRange(new Gray(StartRangeH), new Gray(EndRangeH));
-                //if (Colour == SignColour.RED)
-                //{
-                //    mask._Not();
-                //}
-                //channels[1]._ThresholdBinary(new Gray(100), new Gray(255.0));
-                //channels[2]._ThresholdBinary(new Gray(StartRangeV), new Gray(255.0));
-                //mask._And(channels[1]);
-                //mask._And(channels[2]);
-
-                //return mask;
+                StartRangeH = 100;
+                EndRangeH = 135;
+                StartRangeS = 110;
+                StartRangeV = 30;
             }
-            else
+
+            if (Colour == SignColour.RED)
             {
-                int StartRangeC = 0;
-                int EndRangeC = 20;
-                int StartRangeM = 0;
-                int EndRangeM = 20;
-                int StartRangeY = 0;
-                int EndRangeY = 20;
-                int StartRangeK = 0;
-                int EndRangeK = 0;
-
-                if (Colour == SignColour.RED)
+                StartRangeH = 10;
+                EndRangeH = 170;
+                StartRangeS = 50;
+                StartRangeV = 10;
+            }
+            using (Image<Hsv, Byte> hsv = image.Convert<Hsv, Byte>())
+            {
+                Image<Gray, Byte>[] channels = hsv.Split();
+                try
                 {
-                    StartRangeC = 10;
-                    EndRangeC = 255;
-                    StartRangeY = 0;
-                    EndRangeY = 20;
+                    CvInvoke.cvInRangeS(channels[0], new MCvScalar(StartRangeH), new MCvScalar(EndRangeH), channels[0]);
+                    if (Colour == SignColour.RED)
+                    {
+                        channels[0]._Not();
+                    }
+                    channels[1]._ThresholdBinary(new Gray(StartRangeS), new Gray(255.0));
+                    channels[2]._ThresholdBinary(new Gray(StartRangeV), new Gray(255.0));
+                    CvInvoke.cvAnd(channels[0], channels[1], channels[0], IntPtr.Zero);
+                    CvInvoke.cvAnd(channels[0], channels[2], channels[0], IntPtr.Zero);
                 }
-
-                if (Colour == SignColour.BLUE)
+                finally
                 {
-                    StartRangeC = 0;
-                    EndRangeC = 20;
-                    StartRangeY = 10;
-                    EndRangeY = 255;
-                    StartRangeK = 0;
-                    EndRangeK = 0;
+                    channels[1].Dispose();
+                    channels[2].Dispose();
                 }
-                // Needs revision: unessary components
-                Image<Bgr, Byte> bgr = image;
-                Image<Gray, Byte>[] bgrChannels = bgr.Split();
-
-                Image<Gray, Byte> filterK = bgrChannels[0].CopyBlank();
-                Image<Gray, Byte> filterC = bgrChannels[0].CopyBlank();
-                Image<Gray, Byte> filterM = bgrChannels[0].CopyBlank();
-                Image<Gray, Byte> filterY = bgrChannels[0].CopyBlank();
-
-                Image<Gray, Byte> oneMinBlue = bgrChannels[0];
-                oneMinBlue._Not();
-                Image<Gray, Byte> oneMinGreen = bgrChannels[1];
-                oneMinGreen._Not();
-                Image<Gray, Byte> oneMinRed = bgrChannels[2];
-                oneMinRed._Not();
-
-                CvInvoke.cvMin(oneMinBlue, oneMinGreen, filterK);
-                CvInvoke.cvMin(filterK, oneMinRed, filterK);
-
-                Image<Gray, Byte> oneMinRedMinBlack = bgrChannels[0].CopyBlank();
-                CvInvoke.cvSub(oneMinRed, filterK, oneMinRedMinBlack, IntPtr.Zero);
-                Image<Gray, Byte> oneMinGreenMinBlack = bgrChannels[0].CopyBlank();
-                CvInvoke.cvSub(oneMinGreen, filterK, oneMinGreenMinBlack, IntPtr.Zero);
-                Image<Gray, Byte> oneMinBlueMinBlack = bgrChannels[0].CopyBlank();
-                CvInvoke.cvSub(oneMinBlue, filterK, oneMinBlueMinBlack, IntPtr.Zero);
-                Image<Gray, Byte> oneMinBlack = filterK;
-                oneMinBlack._Not();
-
-                CvInvoke.cvDiv(oneMinRedMinBlack, oneMinBlack, filterC, 255);
-                CvInvoke.cvDiv(oneMinGreenMinBlack, oneMinBlack, filterM, 255);
-                CvInvoke.cvDiv(oneMinBlueMinBlack, oneMinBlack, filterY, 255);
-
-                filterC = filterC.InRange(new Gray(StartRangeC), new Gray(EndRangeC));
-                filterC._Not();
-                filterM = filterM.InRange(new Gray(StartRangeM), new Gray(EndRangeM));
-                filterM._Not();
-                filterY = filterY.InRange(new Gray(StartRangeY), new Gray(EndRangeY));
-                filterY._Not();
-                filterK = filterK.InRange(new Gray(StartRangeK), new Gray(EndRangeK));
-                filterK._Not();
-
-                Image<Gray, byte> mask = filterC.CopyBlank();
-                CvInvoke.cvAnd(filterC, filterM, mask, IntPtr.Zero);
-                CvInvoke.cvAnd(mask, filterY, mask, IntPtr.Zero);
-                CvInvoke.cvAnd(mask, filterK, mask, IntPtr.Zero);
-
-                // Free memory
-                //bgr.Dispose();
-                //for (int i = 0; i < 3; i++) bgrChannels[i].Dispose();
-                //oneMinBlue.Dispose();
-                //oneMinGreen.Dispose();
-                //oneMinRed.Dispose();
-                //oneMinRedMinBlack.Dispose();
-                //oneMinGreenMinBlack.Dispose();
-                //oneMinBlueMinBlack.Dispose();
-                //oneMinBlack.Dispose();
-                //filterC.Dispose();
-                //filterY.Dispose();
-                //filterM.Dispose();
-                //filterK.Dispose();
-
-                return mask;
+                return channels[0];
             }
         }
 
